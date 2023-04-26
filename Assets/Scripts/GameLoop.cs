b7d87d63a895b4f454066ac9e806e2f3678657e9
afshirazi <ashirazi@purdue.edu>
2023-04-26 19:30:03 -0400

    animation

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -23,8 +23,9 @@ public class GameLoop : MonoBehaviour
 
     IEnumerator SetAndPlaySound() {
         while (true){
+            yield return new WaitForSeconds(3);
             asc.SetCustomerState(0); // idle
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSeconds(2);
             int kana = (int)Random.Range(0, 4);
             PPScript.SetKanaTag("kana_a");
             CurrSound.clip = KanaSounds[kana];
